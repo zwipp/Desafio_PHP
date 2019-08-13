6e75d4b58ca1211f402e848b9e964810bba17e6c
zwipp-pc <nuneszwipp@hotmail.com>
2019-08-13 17:00:43 -0300

    TA PRONTO FUNCIONALMENTE GARAIIIIIIII

--- cadastro.php
+++ cadastro.php
@@ -67,7 +67,6 @@
 <body>
 
 	<nav class="navbar navbar-expand-lg navbar-light bg-light">
-  		<a class="navbar-brand" href="#">Navbar w/ text</a>
   		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
     		<span class="navbar-toggler-icon"></span>
   		</button>
--- cadastro_usuario.php
+++ cadastro_usuario.php
@@ -55,7 +55,6 @@
 <body>
     
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  		<a class="navbar-brand" href="#">Navbar w/ text</a>
   		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
     		<span class="navbar-toggler-icon"></span>
   		</button>
